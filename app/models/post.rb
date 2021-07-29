9fac6368b80522e6667adfee399f289b5add0b13
“Pavel <itspavelnosarev@gmail.com>
2021-07-29 11:56:54 -0400

    hw

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  attr_accessible :content, :name, :title
+  validates :name, :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+end
